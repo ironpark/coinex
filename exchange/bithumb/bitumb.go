ddfdbd5cd9d9296290d0e604a578be247da10b3a
ironpark <cjfdhksaos@gmail.com>
2017-05-22 15:52:16 +0900

    bithumb public apis (not tested)

--- exchange/bithumb/bitumb.go
+++ exchange/bithumb/bitumb.go
@@ -4,14 +4,11 @@ import (
 	"net/http"
 	"io/ioutil"
 	"encoding/json"
-	"time"
 	//"fmt"
+	"github.com/iris-contrib/errors"
 )
 
 type EX_Bitumb struct {
-	updateTarget []string
-	updateCallback func (*EX_Bitumb,string)
-
 	Prices map[string]float64
 	apiKey string
 	apiKeySecret string
@@ -20,64 +17,83 @@ type EX_Bitumb struct {
 
 }
 
-
 func NewEXBitumb() (*EX_Bitumb) {
 	c := &EX_Bitumb{
 		reqTime: 10,
-		updateTarget:[]string{"BTC","ETH"},
-		updateCallback:nil,
 		Prices:map[string]float64{},
 	}
 
 	return c
 }
-func  (ex *EX_Bitumb) SetUpdateCall(callback func (*EX_Bitumb,string)) {
-	ex.updateCallback = callback
-}
 
-func  (ex *EX_Bitumb) tickerUpdate(order,payment string) bool {
-	var ticker TickerJsonEXBitumb
-	resp := ex.callJsonApi("/public/ticker/"+order, "")
+func (ex *EX_Bitumb)GetTicker(currency string) (ticker JsonTicker,err error) {
+	resp := ex.callPublicApi("ticker/"+currency)
 	if resp != nil {
 		json.Unmarshal(resp, &ticker)
 	} else {
-		return false
+		errors.New("Fail Call Api")
 	}
-
-	if int(ex.Prices[order]) == int(ticker.Data.ClosingPrice){
-		return false
+	err = ex.getError(ticker.Status)
+	if err != nil {
+		return nil,err
 	}
-	ex.Prices[order] = ticker.Data.ClosingPrice
-	return true
+	return ticker,err
 }
 
-func  (ex *EX_Bitumb) updatePrices() {
+func (ex *EX_Bitumb)GetTransactions(currency string) (transaction JsonRecentTransaction,err error) {
+	resp := ex.callPublicApi("recent_transactions/"+currency)
+	if resp != nil {
+		json.Unmarshal(resp, &transaction)
+	} else {
+		errors.New("Fail Call Api")
+	}
+	err = ex.getError(transaction.Status)
+	if err != nil {
+		return nil,err
+	}
+	return transaction,err
+}
 
-	for _,e := range ex.updateTarget {
+func (ex *EX_Bitumb)GetOrderbook(currency string) (orderbook JsonOrderbook,err error) {
+	resp := ex.callPublicApi("orderbook/"+currency)
+	if resp != nil {
+		json.Unmarshal(resp, &orderbook)
+	} else {
+		errors.New("Fail Call Api")
+	}
+	err = ex.getError(orderbook.Status)
+	if err != nil {
+		return nil,err
+	}
+	return orderbook,err
+}
 
-		if ex.tickerUpdate(e,"KRW") {
-			if ex.updateCallback != nil {
-				ex.updateCallback(ex, e)
-			}
-		}
 
-		// element is the element from someSlice for where we are
+func (ex*EX_Bitumb) getError(status string)(err error) {
+	switch status {
+	case "5100":
+		err = errors.New("Bad Request")
+	case "5200":
+		err = errors.New("Not Member")
+	case "5300":
+		err = errors.New("Invalid Apikey")
+	case "5302":
+		err = errors.New("Method Not Allowed")
+	case "5400":
+		err = errors.New("Database Fail")
+	case "5500":
+		err = errors.New("Invalid Parameter")
+	case "5600":
+		err = errors.New("CUSTOM NOTICE")
+	case "5900":
+		err = errors.New("Unknown Error")
+	default:
+		return nil
 	}
 }
-func  (ex *EX_Bitumb) UpdateLoop(){
-	for {
-		time.Sleep(10 * time.Second)
-		ex.updatePrices()
-	}
-}
-func  (ex *EX_Bitumb) AutoUpdate() {
-	ex.updatePrices()
-	go ex.UpdateLoop()
-}
 
-
-func  (ex *EX_Bitumb) callJsonApi(endpoint string, params string) ([]byte){
-	var api_url = "https://api.bithumb.com" + endpoint
+func  (ex *EX_Bitumb) callPublicApi(endpoint string) ([]byte){
+	var api_url = "https://api.bithumb.com/public/" + endpoint
 	// Connects to Bithumb API server and returns JSON result value.
 	response, err := http.Get(api_url)
 	contents, err := ioutil.ReadAll(response.Body)
--- exchange/bithumb/json.go
+++ exchange/bithumb/json.go
@@ -1,32 +1,75 @@
 package bithumb
 
 //JSON DataStruct
-type TickerJsonEXBitumb struct {
+//Public Apis
+
+type JsonTicker struct {
+	Data struct {
+		AveragePrice string `json:"average_price"`
+		ClosingPrice string `json:"closing_price"`
+		Date         int    `json:"date"`
+		MaxPrice     string `json:"max_price"`
+		MinPrice     string `json:"min_price"`
+		OpeningPrice string `json:"opening_price"`
+		UnitsTraded  string `json:"units_traded"`
+		Volume1day   string `json:"volume_1day"`
+		Volume7day   string `json:"volume_7day"`
+	} `json:"data"`
 	Status string `json:"status"`
-	Data TickerDataEXBitumb `json:"data"`
 }
 
-type TickerDataEXBitumb struct {
-	OpeningPrice float64 `json:"opening_price,string"`
-	ClosingPrice float64 `json:"closing_price,string"`
-	Min_price float64 `json:"min_price,string"`
-	Max_price float64 `json:"max_price,string"`
-	Average_price  float64 `json:"average_price,string"`
-	Units_traded float64 `json:"units_traded,string"`
-	Volume1day float64 `json:"volume_1day,string"`
-	Volume7day float64 `json:"volume_7day,string"`
-	Date int64 `json:"date,int"`
-
-}
-
-// Account JSON structure
-type AccountInfoEXBitumb  struct {
+type JsonOrderbook struct {
+	Data struct {
+		Asks []struct {
+			Price    string `json:"price"`
+			Quantity string `json:"quantity"`
+		} `json:"asks"`
+		Bids []struct {
+			Price    string `json:"price"`
+			Quantity string `json:"quantity"`
+		} `json:"bids"`
+		OrderCurrency   string `json:"order_currency"`
+		PaymentCurrency string `json:"payment_currency"`
+		Timestamp       int    `json:"timestamp"`
+	} `json:"data"`
 	Status string `json:"status"`
-	Data AccountInfoDataEXBitumb `json:"data"`
 }
-type AccountInfoDataEXBitumb struct {
-	Created int64 `json:"created,string"`
-	Account_id string `json:"account_id"`
-	Trade_fee float64 `json:"trade_fee,string"`
-	Balance float64 `json:"balance,string"`
+
+type JsonRecentTransaction struct {
+	Data []struct {
+		Price           string `json:"price"`
+		Total           string `json:"total"`
+		TransactionDate string `json:"transaction_date"`
+		Type            string `json:"type"`
+		UnitsTraded     string `json:"units_traded"`
+	} `json:"data"`
+	Status string `json:"status"`
+}
+
+//Private Apis
+type JsonAccount struct {
+	Data struct {
+		AccountID string `json:"account_id"`
+		Balance   string `json:"balance"`
+		Created   int    `json:"created"`
+		TradeFee  string `json:"trade_fee"`
+	} `json:"data"`
+	Status string `json:"status"`
+}
+
+
+type JsonBalance struct {
+	Data struct {
+		AvailableBtc string `json:"available_btc"`
+		AvailableKrw string `json:"available_krw"`
+		InUseBtc     string `json:"in_use_btc"`
+		InUseKrw     string `json:"in_use_krw"`
+		MisuBtc      string `json:"misu_btc"`
+		MisuDepoKrw  string `json:"misu_depo_krw"`
+		MisuKrw      string `json:"misu_krw"`
+		TotalBtc     string `json:"total_btc"`
+		TotalKrw     string `json:"total_krw"`
+		XcoinLast    string `json:"xcoin_last"`
+	} `json:"data"`
+	Status string `json:"status"`
 }
